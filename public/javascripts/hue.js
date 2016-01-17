0d987377f7e6b1bc32d97cb097fcadb28cbe061d
TonyMcKendry <TonyMcKendry@gmail.com>
2016-01-17 16:38:34 -0700

    colors

--- public/javascripts/hue.js
+++ public/javascripts/hue.js
@@ -1,15 +1,52 @@
-console.log("This Works")
+function hueMaker(){
+  var rand = Math.round((Math.random() * 4))
+  if (rand > 3){
+    // var a = Math.round((Math.random() * 3 + 1) * 50)
+    // var b = 255
+    var a = 0
+    var b = 126
+    var c = 255
+  }
+  else if (rand > 2){
+    var a = 255
+    var b = 0
+    var c = 126
+    // var b = 255
+    // var c = Math.round((Math.random() * 3 + 1) * 50)
+  }
+  else if (rand > 1) {
+    var a = 0
+    var b = 255
+    var c = 126
+    // var a = 255
+    // var b = Math.round((Math.random() * 3 + 1) * 50)
+    // var c = 255
+  }
 
-var hue
+  else {
+    var a = 255
+    var b = 255
+    var c = 126
+  }
+  return 'rgb(' + (Math.floor((256-199)*Math.random()) + a) + ','
+                 + (Math.floor((256-199)*Math.random()) + b) + ','
+                 + (Math.floor((256-199)*Math.random()) + c) + ')';
+}
 
 $(".hue").mouseenter(function(){
-  hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ','
-                 + (Math.floor((256-199)*Math.random()) + 200) + ','
-                 + (Math.floor((256-199)*Math.random()) + 200) + ')';
+  var hue = hueMaker()
   // $(this).animate({backgroundColor: "#aa0000"}, 500);
   $(this).css("background-color", hue)
 })
 
-$("div").mouseleave(function(){
-  $(this).css({"background-color": "white"}, 500)
+$(".hue").mouseleave(function(){
+  $(this).css({"background": "transparent"}, 2000)
+})
+
+$("#drop li").mouseenter(function(){
+  var hue = hueMaker()
+  $(this).css("color", hue)
+})
+$("#drop li").mouseleave(function(){
+  $(this).css("color", "black")
 })
